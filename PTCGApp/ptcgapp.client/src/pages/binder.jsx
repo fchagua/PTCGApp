bdb6473bbbd3c3fcc6c24278a9c8ebb73dc98b08
Francisco Chagua <fchagua@purdue.edu>
2024-10-08 16:25:24 -0500

    Experiment with Pokemon TCG Api all cards search

--- PTCGApp/ptcgapp.client/src/pages/binder.jsx
+++ PTCGApp/ptcgapp.client/src/pages/binder.jsx
@@ -10,19 +10,28 @@ function Binder() {
 
     //search bar data
     const [query, setQuery] = useState('');
-    const [searchData, setSearchData] = useState(null);
+    const [searchData, setSearchData] = useState([]);
     const [searchImage, setSearchImage] = useState(null);
+
+    //all cards
+    const [cards, setCards] = useState([]);
 
     useEffect(() => {
         const fetchPokemon = async () => {
             try {
                 const response = await fetch(' https://api.pokemontcg.io/v2/cards/xy1-1');
+                const cardsResponse = await fetch(' https://api.pokemontcg.io/v2/cards');
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
                 }
                 const result = await response.json();
+                const cardsResult = await cardsResponse.json();
                 setData(result);
                 setImage(result.data.images.small);
+
+                setCards(cardsResult);
+                //console.log(result);
+                //console.log(cardsResult);
             } catch (error) {
                 setError(error);
             } finally {
@@ -45,13 +54,15 @@ function Binder() {
         setQuery(query);
         const searchCard = async () => {
             try {
-                const response = await fetch(`https://api.pokemontcg.io/v2/cards/${query}`);
+                const response = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${query}*`);
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
                 }
                 const result = await response.json();
                 setSearchData(result);
                 setSearchImage(result.data.images.small);
+                console.log(searchData);
+                console.log(searchImage);
             } catch (error) {
                 setError(error);
             } finally {
@@ -70,6 +81,16 @@ function Binder() {
                 onChange={(e) => handleSearch(e.target.value)}
                 placeholder="Search..."
             />
+            <h1>Card Search</h1>
+            {Array.isArray(cards) && cards.length > 0 ? (
+                cards.map((item, index) => (
+                    <div key={index}>
+                        <p>{item.name}</p>
+                    </div>
+                ))
+            ) : (
+                <p>No data found</p>
+            )}
             <h1>Data:</h1>
             {image ? <img src={image} alt="Fetched from API" /> : <div>No image available</div>}
             <pre>{JSON.stringify(data, null, 2)}</pre>
